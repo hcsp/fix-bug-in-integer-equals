a74d941a768f9024f7beac68fcc12457634edabb
吕子威 <18500905537@163.com>
2019-08-02 10:42:37 +0800

    装箱类型应该用equal比较，或者转为Int (#41)
    装箱类型应该用equal比较，或者转为Int (#41)

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -5,12 +5,14 @@ public class Main {
         // 这里应该输出两个true，实际输出的却是两个false
         // 请查找、思考并修复numberEquals方法中的问题
         System.out.println(numberEquals("1234", "+1234"));
-        System.out.println(numberEquals("1234", "1234"));
+//        System.out.println(numberEquals("1234", "1234"));
     }
 
     // 判断两个字符串是否包含相等的数字
     // 例如a为"+1234"，b为"1234"，返回true
     public static boolean numberEquals(String a, String b) {
-        return Integer.valueOf(a) == Integer.valueOf(b);
+        int a2 = Integer.valueOf(a);
+        int b2 = Integer.valueOf(b);
+        return a2 == b2;
     }
 }
