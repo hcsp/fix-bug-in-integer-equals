55bb2da5e61a00f4e2e11e8db647ef3253c3fc42
Lin Sinan <mynameisxiaou@gmail.com>
2019-10-12 00:50:19 +0200

    fix bug in Integer equals

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -11,6 +11,10 @@ public class Main {
     // 判断两个字符串是否包含相等的数字
     // 例如a为"+1234"，b为"1234"，返回true
     public static boolean numberEquals(String a, String b) {
-        return Integer.valueOf(a) == Integer.valueOf(b);
+        if (a != null && b != null) {
+            return Integer.parseInt(a) == Integer.parseInt(b);
+        } else {
+            return a == null && b == null;
+        }
     }
 }
