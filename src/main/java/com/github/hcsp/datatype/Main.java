3bbedb55b09b8c5014f69d2af71f80f54e3d78d1
dqprecisian <1352034864@qq.com>
2019-10-13 17:17:22 +0800

    修复两个Integer相比较的问题，记住Integer类型的变量是对象。

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -6,11 +6,21 @@ public class Main {
         // 请查找、思考并修复numberEquals方法中的问题
         System.out.println(numberEquals("1234", "+1234"));
         System.out.println(numberEquals("1234", "1234"));
+
+        String str1 = "a";
+        String str2 = "a";
+
+        System.out.println(str1 == str2);
+
+        Integer in1 = 1000;
+        Integer in2 = 1000;
+
+        System.out.println(in1 == in2);
     }
 
     // 判断两个字符串是否包含相等的数字
     // 例如a为"+1234"，b为"1234"，返回true
     public static boolean numberEquals(String a, String b) {
-        return Integer.valueOf(a).intValue() == Integer.valueOf(b).intValue();
+        return Integer.valueOf(a).equals(Integer.valueOf(b));
     }
 }
