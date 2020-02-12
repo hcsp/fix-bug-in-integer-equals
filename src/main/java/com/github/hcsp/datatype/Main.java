3a8481dfc70057c4a6188f5a506fa8f150a103e6
ljb99 <ljb9962@gmai.com>
2020-02-11 19:03:56 -0800

    修复比较整数时的bug

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -6,11 +6,12 @@ public class Main {
         // 请查找、思考并修复numberEquals方法中的问题
         System.out.println(numberEquals("1234", "+1234"));
         System.out.println(numberEquals("1234", "1234"));
+
     }
 
     // 判断两个字符串是否包含相等的数字
     // 例如a为"+1234"，b为"1234"，返回true
     public static boolean numberEquals(String a, String b) {
-        return Integer.valueOf(a) == Integer.valueOf(b);
+        return Integer.valueOf(a).equals(Integer.valueOf(b));
     }
 }
