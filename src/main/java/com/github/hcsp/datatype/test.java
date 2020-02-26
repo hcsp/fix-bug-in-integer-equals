a7280abab9f6a12a0d9b031cbbee923d68a2d2c2
pathjh <1639925487@qq.com>
2020-02-26 18:55:54 +0800

    提取字符串数字进行比较

---
+++ src/main/java/com/github/hcsp/datatype/test.java
@@ -0,0 +1,10 @@
+package com.github.hcsp.datatype;
+
+public class test {
+    public static void main(String[] args) {
+        // 这里应该输出两个true，实际输出的却是两个false
+        // 请查找、思考并修复numberEquals方法中的问题
+        System.out.println(Integer.valueOf("-123"));
+
+    }
+}
