15f9629e6be13a7c3b2a5f04b6d1c11d441d5ddb
samuel <zhangleven@gmail.com>
2020-03-12 04:23:58 +0800

    fix integer in string equals

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -11,6 +11,7 @@ public class Main {
     // 判断两个字符串是否包含相等的数字
     // 例如a为"+1234"，b为"1234"，返回true
     public static boolean numberEquals(String a, String b) {
-        return Integer.valueOf(a) == Integer.valueOf(b);
+        if (null == a || null == b) return false;
+        return Integer.parseInt(a) == Integer.parseInt(b);
     }
 }
