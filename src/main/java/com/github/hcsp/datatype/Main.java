0dc7547cea4cc9323c37f61e3d9c5f193898a91f
JoeyPine <fanjiasong_7@outlook.com>
2020-03-22 21:43:21 +0800

    修复比较String类型的整数时的bug

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -11,6 +11,9 @@ public class Main {
     // 判断两个字符串是否包含相等的数字
     // 例如a为"+1234"，b为"1234"，返回true
     public static boolean numberEquals(String a, String b) {
-        return Integer.valueOf(a) == Integer.valueOf(b);
+        if(b.contains(a)){
+            return true;
+        }
+        return a.equals(b);
     }
 }
