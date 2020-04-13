f340966dc0d79d920bb4f3b8226606e84fed2b2b
yujie-zhang <907420318@qq.com>
2020-04-13 13:12:24 +0800

    1

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -1,19 +1,16 @@
 package com.github.hcsp.datatype;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-
 public class Main {
     public static void main(String[] args) {
-//         这里应该输出两个true，实际输出的却是两个false
-//         请查找、思考并修复numberEquals方法中的问题
+        // 这里应该输出两个true，实际输出的却是两个false
+        // 请查找、思考并修复numberEquals方法中的问题
         System.out.println(numberEquals("1234", "+1234"));
         System.out.println(numberEquals("1234", "1234"));
     }
 
-        // 判断两个字符串是否包含相等的数字
-        // 例如a为"+1234"，b为"1234"，返回true
+    // 判断两个字符串是否包含相等的数字
+    // 例如a为"+1234"，b为"1234"，返回true
     public static boolean numberEquals(String a, String b) {
         return Integer.valueOf(a).equals(Integer.valueOf(b));
     }
-
 }
