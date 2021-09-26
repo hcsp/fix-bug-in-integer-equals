237d6579246fa559fccef7259ef23280b60cf2a5
AdFeng <3200185362@qq.com>
2021-09-26 16:08:11 +0800

    equals也可以解决

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -11,6 +11,8 @@ public class Main {
     // 判断两个字符串是否包含相等的数字
     // 例如a为"+1234"，b为"1234"，返回true
     public static boolean numberEquals(String a, String b) {
-        return Integer.valueOf(a) == Integer.valueOf(b);
+        int c = new Integer(a);
+        int d = new Integer(b);
+        return c == d;
     }
 }
