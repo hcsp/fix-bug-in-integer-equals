e05347b346428e07c8b20f84f02d075a66fc9e8d
Lyraaa <Lyraaaaaaaaaaa@gmail.com>
2023-10-29 23:42:25 +0800

    3 (#297)
    3 (#297)

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -1,16 +1,19 @@
 package com.github.hcsp.datatype;
-
-public class Main {
-    public static void main(String[] args) {
+import java.util.HashMap;
+public class Main {                                     //main类
+    public static void main(String[] args) {            //程序入口
         // 这里应该输出两个true，实际输出的却是两个false
         // 请查找、思考并修复numberEquals方法中的问题
-        System.out.println(numberEquals("1234", "+1234"));
+        System.out.println(numberEquals("1234", "+1234"));          //方法调用
         System.out.println(numberEquals("1234", "1234"));
     }
 
     // 判断两个字符串是否包含相等的数字
     // 例如a为"+1234"，b为"1234"，返回true
-    public static boolean numberEquals(String a, String b) {
-        return Integer.valueOf(a) == Integer.valueOf(b);
+    public static boolean numberEquals(String a, String b) {//方法声明
+        int m = Integer.valueOf(a);
+        int n = Integer.valueOf(b);
+        return m == n;
     }
 }
+
